f599751f66e346ed7a47803c981a8ef97cc0298f
MelnikovDenis <sinennn@yandex.ru>
2023-05-03 00:33:31 +0500

    Создал WPF-проект для 15 лабы, (вывод графика работает, но сам график неправильный. НАДО ИСПРАВИТЬ)

---
+++ Lab15/FunctionGraph.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Lab15
+{
+    /// <summary>
+    /// График функции
+    /// </summary>
+    internal class FunctionGraph : DependencyObject, INotifyPropertyChanged
+    {   
+        public static readonly DependencyProperty PointsProperty = DependencyProperty.Register(
+            "Points",
+            typeof(ObservableCollection<Point>),
+            typeof(MainWindow),
+            new PropertyMetadata(
+                default(ObservableCollection<Point>)
+                )
+            );
+        /// <summary>
+        /// Список точек
+        /// </summary>
+        public ObservableCollection<Point> Points
+        {
+            get { return (ObservableCollection<Point>)GetValue(PointsProperty); }
+            set { 
+                SetValue(PointsProperty, value); 
+                OnPropertyChanged("Points");
+            }
+        }    
+        public static readonly DependencyProperty AccuracyProperty = DependencyProperty.Register(
+          "Accuracy", 
+          typeof(double), 
+          typeof(MainWindow), 
+          new PropertyMetadata(0.01d)
+            );
+        /// <summary>
+        /// Точность (шаг)
+        /// </summary>
+        public double Accuracy
+        {
+            get { return (double)GetValue(AccuracyProperty); }
+            set { 
+                SetValue(AccuracyProperty, value);
+                OnPropertyChanged("Accuracy");
+            }
+        }    
+        public static readonly DependencyProperty RangeToProperty = DependencyProperty.Register(
+        "RangeTo", typeof(double), typeof(MainWindow), new PropertyMetadata(50d));
+        /// <summary>
+        /// Диапозон значений
+        /// </summary>
+
+        public double RangeTo
+        {
+            get { return (double)GetValue(RangeToProperty); }
+            set { 
+                SetValue(RangeToProperty, value);
+                OnPropertyChanged("RangeTo");
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ Lab15/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab15
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        FunctionGraph graph = new FunctionGraph();
+        public MainWindow()
+        {
+            InitializeComponent();           
+            this.DataContext = graph;
+        }
+        private void Start_Button_Click(object sender, RoutedEventArgs e)
+        {
+            List<Point> points = new List<Point>();
+            
+            for(double x = graph.RangeTo * -1; x <= graph.RangeTo; x += graph.Accuracy) 
+            {
+                points.Add(new Point(x, 23d * Math.Pow(x, 2) - 32));
+            }
+            
+            graph.Points = new ObservableCollection<Point>(points);
+        }
+    }
+}
